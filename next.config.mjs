7c62d0406975a3eba3e45f6f0e56d23b1bb1c4ad
jayuparmar <153155790+jayuparmar@users.noreply.github.com>
2025-07-08 18:17:28 +0530

    Update next.config.mjs
    update config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains:[
+            "us.abbottlyon.com",
+            "cleopatrajewelers.com",
+            "www.proclamationjewelry.com",
+            "www.sabbia.com",
+            "images.unsplash.com",
+        ]
+    },
+    async redirects() {
+        return [
+            {
+                source: '/dashboard',
+                destination: '/',
+                permanent: true,
+            },
+        ]
+    },
+};
 
 export default nextConfig;
